e5fb93a3bb9d4c3ce691f2262f00c27a3efdfda2
SanjaySinha <sanjayk.sinha@nic.in>
2022-06-14 15:13:57 +0530

    Create H1.py

---
+++ H1.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import hydralit_components as hc
+
+#can apply customisation to almost all the properties of the card, including the progress bar
+theme_bad = {'bgcolor': '#FFF0F0','title_color': 'red','content_color': 'red','icon_color': 'red', 'icon': 'fa fa-times-circle'}
+theme_neutral = {'bgcolor': '#f9f9f9','title_color': 'orange','content_color': 'orange','icon_color': 'orange', 'icon': 'fa fa-question-circle'}
+theme_good = {'bgcolor': '#EFF8F7','title_color': 'green','content_color': 'green','icon_color': 'green', 'icon': 'fa fa-check-circle'}
+
+cc = st.columns(4)
+
+with cc[0]:
+ # can just use 'good', 'bad', 'neutral' sentiment to auto color the card
+ hc.info_card(title='Some heading GOOD', content='All good!', sentiment='good',bar_value=77)
+
+with cc[1]:
+ hc.info_card(title='Some BAD BAD', content='This is really bad',bar_value=12,theme_override=theme_bad)
+
+with cc[2]:
+ hc.info_card(title='Some NEURAL', content='Oh yeah, sure.', sentiment='neutral',bar_value=55)
+
+with cc[3]:
+ #customise the the theming for a neutral content
+ hc.info_card(title='Some NEURAL',content='Maybe...',key='sec',bar_value=5,theme_override=theme_neutral)
