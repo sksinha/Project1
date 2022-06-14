ac731d23665834c71338cb229a844eb31664f1f4
SanjaySinha <sanjayk.sinha@nic.in>
2022-06-14 15:50:59 +0530

    Update H1.py

--- H1.py
+++ H1.py
@@ -34,3 +34,24 @@ with cc[2]:
 with cc[3]:
  #customise the the theming for a neutral content
  hc.info_card(title='Some NEURAL',content='Maybe...',key='sec',bar_value=5,theme_override=theme_neutral)
+ #####
+import streamlit as st
+import hydralit_components as hc
+
+# define what option labels and icons to display
+option_data = [
+   {'icon': "bi bi-hand-thumbs-up", 'label':"Agree"},
+   {'icon':"fa fa-question-circle",'label':"Unsure"},
+   {'icon': "bi bi-hand-thumbs-down", 'label':"Disagree"},
+]
+
+# override the theme, else it will use the Streamlit applied theme
+over_theme = {'txc_inactive': 'white','menu_background':'purple','txc_active':'yellow','option_active':'blue'}
+font_fmt = {'font-class':'h2','font-size':'150%'}
+
+# display a horizontal version of the option bar
+op = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=True)
+
+# display a version version of the option bar
+op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
+
